a72c49a2478a22e531cb639d73ca148c8528fbf0
jonathanRQ99 <42821701+jonathanRQ99@users.noreply.github.com>
2019-05-16 13:51:49 -0500

    Add files via upload

--- congreso/Taller.js
+++ congreso/Taller.js
@@ -1,25 +1,55 @@
 export default class Taller {
     constructor(nombre, fechainicio, fechatermino) {
         this._nombre = nombre.toUpperCase();
-        this._fechainicio = fechainicio;
-        this._fechatermino = fechatermino;
-        this._lugaresdisponibles = lugaresdisponibles;
+        this._fechaI = fechaI;
+        this._fechaF = fechaF;
+        this._lugaresD = lugaresD;
+        this._duracion = duracion;
+        this._months = [
+            "Ene",
+            "Feb",
+            "Mar",
+            "Abr",
+            "May",
+            "Jun",
+            "Jul",
+            "Ago",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dic"
+
+        ]
     }
     
     get nombre() {
         return this._nombre;
     }
-
-    get fechainicio() {
-        return this._fechainicio;
+    get fechaI() {
+        return this._fechaI;
     }
-
-    get fechatermino() {
-        return this._fechatermino;
+    get fechaF() {
+        return this._fechaF;
     }
-
-    get lugaresdisponibles() {
-        return this._lugaresdisponibles;
+    get lugaresD() {
+        return this._lugaresD;
     }
-
+    get duracion() {
+        return this._duracion;
+    }
+    getFechaInicioString(){
+        let date = 
+        this._fechaI.getDate() + "/" +
+        this._months[this._fechaI.getMonth()] + "/" +
+        this._fechaI.getFullYear();
+        return date  
+    }
+    getFechaFinalString(){
+        let date = 
+        this._fechaF.getDate() + "/" +
+        this._months[this._fechaF.getMonth()] + "/" +
+        this._fechaF.getFullYear();
+        return date  
+    }
+    
 }
